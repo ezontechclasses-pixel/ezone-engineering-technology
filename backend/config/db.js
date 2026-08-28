e882ddd6ebc1dd1b3173d1f6d41b3201688b774d
Tharindu Jananuvan <tharindujananuvan@gmail.com>
2026-08-28 21:32:39 +0530

    Fix Windows Node SRV DNS lookup for MongoDB Atlas

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,10 @@
 const mongoose = require('mongoose');
+const dns = require('dns');
+
+// Fix Node.js SRV record lookup issue on some Windows/ISP DNS resolvers
+try {
+  dns.setServers(['8.8.8.8', '1.1.1.1']);
+} catch (e) {}
 
 const connectDB = async () => {
   try {
